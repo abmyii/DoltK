08ed80794fe1946f2ffb2a72ba6fa80e339a6ccb
Abdurrahmaan Iqbal <abdurrahmaaniqbal@hotmail.com>
2021-12-30 20:09:33 +0000

    Refactor to simplify code

--- doltk.py
+++ doltk.py
@@ -3,17 +3,10 @@ import sys
 from Qt import QtWidgets, QtCompat
 
 
-class MainWindow(QtWidgets.QWidget):
-    """Load .ui file example, using setattr/getattr approach"""
-    def __init__(self, parent=None):
-        QtWidgets.QWidget.__init__(self, parent)
-        self.ui = QtCompat.loadUi('ui/mainwindow.ui')
-
-
 def main():
     app = QtWidgets.QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
+    mainwindow = QtCompat.loadUi('ui/mainwindow.ui')
+    mainwindow.show()
     sys.exit(app.exec_())
 
 
