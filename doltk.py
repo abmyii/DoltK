b4e74bd66543622f15dacdc3f86c8dd3a57edfd6
Abdurrahmaan Iqbal <abdurrahmaaniqbal@hotmail.com>
2022-01-31 19:32:40 +0000

    Add tables list

--- doltk.py
+++ doltk.py
@@ -33,7 +33,7 @@ class CommitHistoryModel(QtCore.QAbstractTableModel):
 
     def data(self, index, role):
         self.current_commit = self.history[index.row()]
-       
+
         if role == QtCore.Qt.DisplayRole:
             if index.column() == 0: return self.current_commit.message.replace('\n', ' ')
             if index.column() == 1: return f"{self.current_commit.author} <{self.current_commit.email}>"
@@ -41,7 +41,7 @@ class CommitHistoryModel(QtCore.QAbstractTableModel):
 
     def rowCount(self, index):
         return len(self.history)
-    
+
     def columnCount(self, index):
         return 3
 
@@ -53,6 +53,10 @@ class DiffModel(QtCore.QAbstractTableModel):
     def __init__(self, vertical_header_height, *args, **kwargs):
         self.vertical_header_height = vertical_header_height
         super().__init__(*args, **kwargs)
+
+    @property
+    def tables(self):
+        return list(self.diff.keys())
 
     def load_diff(self, conn, commit):
         self.diff = conn.diff(commit.parents, commit.ref, conn.tables())
@@ -71,28 +75,41 @@ class DiffModel(QtCore.QAbstractTableModel):
             column = self.diff[self.current_table].columns[section]
 
             if role == QtCore.Qt.DisplayRole:
-                return column 
+                return column
             elif role == QtCore.Qt.SizeHintRole:
                 # https://stackoverflow.com/a/46142682
                 max_len = self.diff[self.current_table][column].astype(str).str.len().max()
-                width = max(len(column)*10, max_len*9)
-                print(column, width)
+                width = max(len(column), max_len)*10  # 10 is font-size?
+                #print(column, width)
                 size = QtCore.QSize(width, self.vertical_header_height)
                 return size
 
     def rowCount(self, index):
         return len(self.diff[self.current_table])
-        #return 100
 
     def columnCount(self, index):
         return len(self.diff[self.current_table].columns)
+
+
+class DiffModelTables(QtCore.QAbstractListModel):
+
+    def __init__(self, diff_model, *args, **kwargs):
+        self.diff_model = diff_model
+        super().__init__(*args, **kwargs)
+
+    def data(self, index, role):
+        if role == QtCore.Qt.DisplayRole:
+            return self.diff_model.tables[index.row()]
+
+    def rowCount(self, index):
+        return len(self.diff_model.tables)
 
 
 class MainWindow:
     def __init__(self):
         # Load repo
         db_path = os.path.normpath('.' if len(sys.argv) < 2 else sys.argv[1])
-        repo = Dolt(db_path) 
+        repo = Dolt(db_path)
 
         # Initialse SQL server and connection
         port = start_sql_server(db_path)
@@ -122,6 +139,9 @@ class MainWindow:
         self.ui.diff.setModel(self.diff_model)
         self.ui.diff.setColumnWidth(0, 60)
         self.ui.diff.resizeColumnsToContents()
+
+        self.diff_model_tables = DiffModelTables(self.diff_model)
+        self.ui.tables.setModel(self.diff_model_tables)
         #self.ui.diff.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
         #self.ui.diff.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.ResizeToContents)
 
