de428d38b0dbcd190b714b68b510c53576614aca
Abdurrahmaan Iqbal <abdurrahmaaniqbal@hotmail.com>
2022-05-18 13:20:52 +0100

    Lint

--- doltk.py
+++ doltk.py
@@ -1,9 +1,6 @@
 import os
 import pandas as pd
-import random
-import subprocess
 import sys
-import time
 
 from doltpy.cli import Dolt
 from doltpy.cli.read import read_table_sql
@@ -13,7 +10,7 @@ from Qt import QtWidgets, QtCompat, QtCore, QtGui
 # NOTE: https://www.dolthub.com/blog/2022-03-25-dolt-diff-magic/
 
 
-CHUNKSIZE = 1000#0
+CHUNKSIZE = 10000
 
 
 # Overrides doltcli.utils.parse_to_pandas since it converts strings to ints
@@ -22,14 +19,20 @@ def parse_to_pandas(sql_output):
 
 
 def get_diff_chunks(repo, table, commit):
-    query = f'SELECT * FROM dolt_diff_{table} WHERE from_commit="{commit.parents}" and to_commit="{commit.ref}" LIMIT {CHUNKSIZE};'
+    query = f"""
+    SELECT * FROM dolt_diff_{table} WHERE
+        from_commit="{commit.parents}" and to_commit="{commit.ref}"
+        LIMIT {CHUNKSIZE};
+    """
     df = read_table_sql(repo, query, result_parser=parse_to_pandas)
 
     # Remove commit info columns
     df = df.drop(df.filter(regex='^(from|to)_commit(_date)*$').columns, axis=1)
 
     # Get table PKs
-    table_columns = read_table_sql(repo, f'DESC {table};', result_parser=parse_to_pandas)
+    table_columns = read_table_sql(
+        repo, f'DESC {table};', result_parser=parse_to_pandas
+    )
     table_pks = list(table_columns[table_columns['Key'] == 'PRI']['Field'])
 
     # Combine from/to columns into one
@@ -39,19 +42,22 @@ def get_diff_chunks(repo, table, commit):
     ]
     for col, from_col, to_col in columns:
         df[col] = df[to_col]
-        df.loc[df['diff_type'] == 'removed', col] = df[from_col]  # For removed rows, to_col is NaN
+        df.loc[df['diff_type'] == 'removed', col] = df[from_col]
 
     # Sort on to_<pk> for all PKs
     df = df.sort_values(table_pks)
 
-    # Add modified overlay - each cell will be a list with two values: [before, after]
-    # NOTE: This is done after sorting as it fails on list values
+    # Modified overlay - each cell will be a list with values: [before, after]
+    # NOTE: This is done after sorting as it fails on list items
     modified = df['diff_type'] == 'modified'
     for col, from_col, to_col in columns:
         df.loc[modified, col] = df[[from_col, to_col]].agg(list, axis=1)
 
+    # Insert modified_to rows directly below original modified row
     df.loc[modified, 'diff_type'] = 'modified_from'
-    df = df.append(df.loc[modified].assign(diff_type='modified_to')).sort_index().reset_index(drop=True)  # Insert rows directly below original modified row
+    df = df.append(
+        df.loc[modified].assign(diff_type='modified_to')
+    ).sort_index().reset_index(drop=True)
 
     # diff_symbols (+ or - depending on diff_type)
     diff_symbols = df['diff_type'].apply(
@@ -85,7 +91,7 @@ class CommitHistoryModel(QtCore.QAbstractTableModel):
             painter = QtGui.QPainter(icon)
             painter.setRenderHint(QtGui.QPainter.Antialiasing)
             painter.setBrush(QtCore.Qt.red)
-            painter.drawEllipse(QtCore.QPoint(8,8),5,5)
+            painter.drawEllipse(QtCore.QPoint(8, 8), 5, 5)
 
             # if has a parent, add line to parent
             if True:  # parent
@@ -98,9 +104,11 @@ class CommitHistoryModel(QtCore.QAbstractTableModel):
         self.current_commit = self.history[index.row()]
 
         if role == QtCore.Qt.DisplayRole:
-            if index.column() == 0: return self.current_commit.message.replace('\n', ' ')
-            if index.column() == 1: return f"{self.current_commit.author} <{self.current_commit.email}>"
-            if index.column() == 2: return self.current_commit.timestamp#.strftime('%Y-%m-%d %H:%M:%S')
+            return {
+                0: self.current_commit.message.replace('\n', ' '),
+                1: f"{self.current_commit.author} <{self.current_commit.email}>",
+                2: self.current_commit.timestamp
+            }[index.column()]
         elif index.column() == 0 and role == QtCore.Qt.DecorationRole:
             return self.icons[index.row()]
 
@@ -125,7 +133,10 @@ class DiffModel(QtCore.QAbstractTableModel):
 
     def load_diff(self, repo, commit):
         # Read first 10k - then only load when scrolling halfway / near end of chunk
-        self.diff = {table.name: get_diff_chunks(repo, table.name, commit) for table in self.tables}
+        self.diff = {
+            table.name: get_diff_chunks(repo, table.name, commit)
+            for table in self.tables
+        }
 
         self.current_table = list(self.diff)[0]
 
@@ -136,10 +147,13 @@ class DiffModel(QtCore.QAbstractTableModel):
     def data(self, index, role):
         row = self.diff[self.current_table].iloc[index.row()]
         value = row.iloc[index.column()]
+
         isna = pd.isna(value) is True
+        mod = len(set(value)) == 2  # Unodified if both values for column are the same
+
         if role == QtCore.Qt.FontRole:
             font = QtGui.QFont("Courier New")
-            font.setStyleHint(QtGui.QFont.Monospace)  # https://stackoverflow.com/a/42042184
+            font.setStyleHint(QtGui.QFont.Monospace)
             return font
         elif role == QtCore.Qt.DisplayRole:
             if isna:
@@ -155,9 +169,9 @@ class DiffModel(QtCore.QAbstractTableModel):
                 return QtGui.QColor('#5AC58D')
             elif row['diff_type'] == 'removed':
                 return QtGui.QColor('#FF9A99')
-            elif row['diff_type'] == 'modified_from' and (len(set(value)) == 2 or index.column() == 0):  # If both values for column are the same, then it is unmodified
+            elif row['diff_type'] == 'modified_from' and (index.column() == 0 or mod):
                 return QtGui.QColor('#FF9A99')
-            elif row['diff_type'] == 'modified_to' and (len(set(value)) == 2 or index.column() == 0):
+            elif row['diff_type'] == 'modified_to' and (index.column() == 0 or mod):
                 return QtGui.QColor('#5AC58D')
             else:
                 return QtGui.QColor('#95A3A7')
@@ -167,7 +181,7 @@ class DiffModel(QtCore.QAbstractTableModel):
             elif row['diff_type'] == 'removed':
                 return QtGui.QColor('#FEE9EB')
         elif role == QtCore.Qt.TextAlignmentRole and index.column() == 0:
-            return int(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)
+            return int(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
 
     def headerData(self, section, orientation, role):
         if orientation == QtCore.Qt.Orientation.Horizontal:
@@ -178,13 +192,14 @@ class DiffModel(QtCore.QAbstractTableModel):
             elif role == QtCore.Qt.SizeHintRole:
                 # https://stackoverflow.com/a/46142682
                 # https://stackoverflow.com/a/27446356 instead?
-                max_len = self.diff[self.current_table][column].astype(str).str.len().max()
+                max_len = \
+                    self.diff[self.current_table][column].astype(str).str.len().max()
                 width = max(len(column), max_len)*10  # 10 is font-size?
-                #print(column, width)
+                # print(column, width)
                 size = QtCore.QSize(width, self.vertical_header_height)
                 return size
             elif role == QtCore.Qt.TextAlignmentRole:
-                return int(QtCore.Qt.AlignLeft|QtCore.Qt.AlignVCenter)
+                return int(QtCore.Qt.AlignLeft | QtCore.Qt.AlignVCenter)
 
     def rowCount(self, index):
         if not self.current_table:
@@ -199,12 +214,14 @@ class DiffModel(QtCore.QAbstractTableModel):
 
 # https://stackoverflow.com/a/39433736
 class HorizontalLineDelegate(QtWidgets.QStyledItemDelegate):
-    
+
     def __init__(self, tableView):
         super().__init__(tableView)
 
-        gridHint = tableView.style().styleHint(QtWidgets.QStyle.SH_Table_GridLineColor, QtWidgets.QStyleOptionViewItem())
-        gridHint &= 0xffffffff  # https://riverbankcomputing.com/pipermail/pyqt/2010-February/025893.html
+        gridHint = tableView.style().styleHint(
+            QtWidgets.QStyle.SH_Table_GridLineColor, QtWidgets.QStyleOptionViewItem()
+        )
+        gridHint &= 0xffffffff  # https://riverbankcomputing.com/pipermail/pyqt/2010-February/025893.html  # noqa: E501
         gridColor = QtGui.QColor.fromRgb(gridHint)
 
         self.pen = QtGui.QPen(gridColor, 0, tableView.gridStyle())
@@ -216,8 +233,12 @@ class HorizontalLineDelegate(QtWidgets.QStyledItemDelegate):
         oldPen = painter.pen()
         painter.setPen(self.pen)
 
-        p1 = QtCore.QPoint(option.rect.bottomLeft().x()-1,option.rect.bottomLeft().y())
-        p2 = QtCore.QPoint(option.rect.bottomRight().x()+1,option.rect.bottomRight().y())
+        p1 = QtCore.QPoint(
+            option.rect.bottomLeft().x()-1, option.rect.bottomLeft().y()
+        )
+        p2 = QtCore.QPoint(
+            option.rect.bottomRight().x()+1, option.rect.bottomRight().y()
+        )
         painter.drawLine(p1, p2)
         painter.setPen(oldPen)
 
@@ -233,7 +254,9 @@ class MainWindow:
         self.ui = QtCompat.loadUi('ui/mainwindow.ui')
 
         # Create commit history model
-        self.commit_views = [self.ui.commit_messages, self.ui.commit_authors, self.ui.commit_timestamps]
+        self.commit_views = [
+            self.ui.commit_messages, self.ui.commit_authors, self.ui.commit_timestamps
+        ]
 
         self.history_model = CommitHistoryModel()
         self.history_model.load_commits(self.repo)
@@ -248,23 +271,33 @@ class MainWindow:
         self.ui.num_commits.setText(str(len(self.history_model.history)))
 
         # Create diff model
-        self.diff_model = DiffModel(self.repo, self.ui.diff.verticalHeader().defaultSectionSize(), self.ui.tables)
+        self.diff_model = DiffModel(
+            self.repo,
+            self.ui.diff.verticalHeader().defaultSectionSize(),
+            self.ui.tables
+        )
 
         self.ui.diff.setModel(self.diff_model)
         self.ui.diff.setColumnWidth(0, 60)
         self.ui.diff.resizeColumnsToContents()
 
         self.ui.tables.currentItemChanged.connect(self.select_table)
-        #self.ui.diff.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
-        #self.ui.diff.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.ResizeToContents)
+        # self.ui.diff.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
+        # self.ui.diff.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.ResizeToContents)
 
         # Style diff table grids
         row_delegate = HorizontalLineDelegate(self.ui.diff)
         self.ui.diff.setItemDelegate(row_delegate)
 
-        self.ui.diff.horizontalHeader().setStyleSheet("""
+        self.ui.diff.horizontalHeader().setStyleSheet(
+            """
             QHeaderView { background-color:white }
-            QHeaderView::section { border: none; border-bottom: 1px solid %s; background-color:white }""" % row_delegate.pen.color().name()
+            QHeaderView::section {
+                border: none;
+                border-bottom: 1px solid %s;
+                background-color:white
+            }
+            """ % row_delegate.pen.color().name()
         )
 
         # Execute
@@ -275,14 +308,15 @@ class MainWindow:
         for index, view in enumerate(self.commit_views):
             model_index = self.history_model.index(current.row(), index)
 
-            # Disable/re-enable signal so we don't trigger on_commit_changed again for each view
+            # Disable/re-enable signal so we don't trigger on_commit_changed
+            # again for each view
             view.selectionModel().currentChanged.disconnect(self.on_commit_changed)
             view.scrollTo(model_index)
             view.setCurrentIndex(model_index)
             view.selectionModel().currentChanged.connect(self.on_commit_changed)
 
         # Load new diff
-        self.diff_model.beginResetModel() 
+        self.diff_model.beginResetModel()
         self.diff_model.load_diff(self.repo, self.history_model.current_commit)
         self.diff_model.endResetModel()
 
@@ -298,7 +332,7 @@ class MainWindow:
 
     def select_table(self, selection):
         if selection:
-            self.diff_model.beginResetModel() 
+            self.diff_model.beginResetModel()
             self.diff_model.current_table = selection.text()  # Update to new table
             self.diff_model.endResetModel()
 
